55bac564c9ff43d9b2011c37fb238931748a5710
juncos22 <juncosnicolas36@gmail.com>
2024-03-19 01:08:43 -0300

    fixing cors

--- api/views.py
+++ api/views.py
@@ -6,19 +6,24 @@ from rest_framework import status
 from django.contrib.auth.models import User
 from api.serializers import UserSerializer
 
+
 class LoginUserAPIView(APIView):
     model = User
+
     def post(self, request, format=None):
         user = get_object_or_404(User, username=request.data['username'])
         if not user.check_password(request.data['password']):
             return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         token, created = Token.objects.get_or_create(user=user)
         serializer = UserSerializer(user)
-        return Response({"token": token.key, "created": created, "userProfile": serializer.data}, status=status.HTTP_200_OK)
+        return Response({"token": token.key, "created": created, "userProfile": serializer.data},
+                        status=status.HTTP_200_OK)
+
 
 class RegisterUserAPIView(APIView):
     model = User
+
     def post(self, request, format=None):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -28,7 +33,7 @@ class RegisterUserAPIView(APIView):
             user.save()
             token = Token.objects.create(user=user)
             return Response(
-                {"token": token.key, 
-                 "loggedUser": serializer.data}, 
+                {"token": token.key,
+                 "loggedUser": serializer.data},
                 status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- turnos/views.py
+++ turnos/views.py
@@ -8,37 +8,40 @@ from rest_framework.authentication import TokenAuthentication
 
 from turnos.models import Turno
 from turnos.serializers import TurnoSerializer
+
+
 # Create your views here.
 
 class TurnosAPIListView(APIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication]
-   
+
     def get(self, request, format=None):
         pacient = get_object_or_404(User, username=request.user.username)
         serializer = TurnoSerializer(pacient.appointments, many=True)
         return Response({'turnos': serializer.data}, status=status.HTTP_200_OK)
-      
+
     def perform_create(self, serializer):
         serializer.save(pacient=self.request.user)
-        
+
     def post(self, request, format=None):
         serializer = TurnoSerializer(data=request.data)
         if serializer.is_valid():
             self.perform_create(serializer=serializer)
             return Response({'data': serializer.data}, status=status.HTTP_201_CREATED)
-        
+
         return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class TurnosDetailView(APIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication]
+
     def get(self, request, pk, format=None):
         turno = get_object_or_404(Turno, pk=pk)
         serializer = TurnoSerializer(instance=turno)
         return Response({"turno": serializer.data}, status=status.HTTP_200_OK)
-    
+
     def put(self, request, pk, format=None):
         turno = get_object_or_404(Turno, pk=pk)
         serializer = TurnoSerializer(turno, data=request.data)
